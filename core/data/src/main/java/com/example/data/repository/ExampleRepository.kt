abb68bc5d23790fcbab7b723be94238e28ce5cce
YuOyama12 <yuoyama.5@gmail.com>
2024-08-16 13:56:28 +0900

    不要なimport文を削除

--- core/data/src/main/java/com/example/data/repository/ExampleRepository.kt
+++ core/data/src/main/java/com/example/data/repository/ExampleRepository.kt
@@ -1,16 +1,12 @@
 package com.example.data.repository
 
-import com.example.database.dao.ExampleDao
-import com.example.database.entity.ExampleEntity
-import com.example.model.Example
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 interface ExampleRepository {
 }
 
 class ExampleRepositoryImpl @Inject constructor(
-    private val exampleDao: ExampleDao,
+    //ローカルDBを使う場合はこのコメントを外す。
+    //private val exampleDao: ExampleDao,
 ) : ExampleRepository {
 }
