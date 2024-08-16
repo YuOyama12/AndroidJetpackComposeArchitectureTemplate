425fc067dd3412f3231a7104eb1fb7a69cb2f061
YuOyama12 <yuoyama.5@gmail.com>
2024-08-16 14:01:15 +0900

    viewModelにusecaseをテンプレートとして置いておく

--- feature/src/main/java/com/example/feature/home/HomeViewModel.kt
+++ feature/src/main/java/com/example/feature/home/HomeViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.feature.home
 
 import androidx.lifecycle.ViewModel
+import com.example.domain.ExampleUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val exampleUseCase: ExampleUseCase
+) : ViewModel() {
 
 }
